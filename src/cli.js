0447645d5b1d4bedc2dc764f7a6a4d28896096ab
meichtor <meichtor@gmail.com>
2025-10-14 16:21:39 +0500

    add format option and args in help cli

--- src/cli.js
+++ src/cli.js
@@ -5,6 +5,9 @@ export default () => {
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
+    .option('-f, --format [type]', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
 
   program.parse()
 }
